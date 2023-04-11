72ca98ee25aaaad5840d01e4e8ba321381dcf9f4
Nathan Arthur <nathan@nathanarthur.com>
2023-04-11 12:21:11 -0400

    figure out event listening

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,4 +1,10 @@
-import { declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
+import {
+  AppEventListerKey,
+  AppEvents,
+  declareIndexPlugin,
+  ReactRNPlugin,
+  WidgetLocation,
+} from '@remnote/plugin-sdk';
 import '../style.css';
 import '../App.css';
 import axios from 'axios';
@@ -43,24 +49,58 @@ async function onActivate(plugin: ReactRNPlugin) {
     options,
   });
 
-  // A command that inserts text into the editor if focused.
-  await plugin.app.registerCommand({
-    id: 'editor-command',
-    name: 'Editor Command',
-    action: async () => {
-      plugin.editor.insertPlainText('Hello World!');
-    },
-  });
+  // // A command that inserts text into the editor if focused.
+  // await plugin.app.registerCommand({
+  //   id: 'editor-command',
+  //   name: 'Editor Command',
+  //   action: async () => {
+  //     plugin.editor.insertPlainText('Hello World!');
+  //   },
+  // });
 
-  // Show a toast notification to the user.
-  await plugin.app.toast("I'm a toast!");
+  // // Show a toast notification to the user.
+  // await plugin.app.toast("I'm a toast!");
 
   // Register a sidebar widget.
   await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
     dimensions: { height: 'auto', width: '100%' },
   });
+
+  // Register event handlers
+  plugin.event.addListener(AppEvents.QueueCompleteCard, undefined, async (e) => {
+    plugin.app.toast('Card completed!');
+    console.log('QueueCompleteCard', e);
+    console.log({
+      streak: '',
+      remaining: '',
+    });
+  });
+
+  plugin.event.addListener(AppEvents.QueueEnter, undefined, async (e) => {
+    plugin.app.toast('Queue entered!');
+    console.log('QueueEnter', e);
+  });
+
+  plugin.event.addListener(AppEvents.QueueLoadCard, undefined, async (e) => {
+    plugin.app.toast('Card loaded!');
+    console.log('QueueLoadCard', e);
+  });
+
+  plugin.event.addListener(AppEvents.QueueExit, undefined, async (e) => {
+    plugin.app.toast('Queue exited!');
+    console.log('QueueExit', e);
+  });
+
+  // useAPIEventListener(AppEvents.QueueCompleteCard, 'bm_queue_complete_card', async (e) => {
+  //   console.log('QueueCompleteCard', e);
+  // });
 }
 
-async function onDeactivate(_: ReactRNPlugin) {}
+async function onDeactivate(plugin: ReactRNPlugin) {
+  // plugin.event.removeListener('QueueCompleteCard', 'bm-queue-complete-card');
+  // plugin.event.removeListener('QueueEnter', 'bm-queue-enter');
+  // plugin.event.removeListener('QueueLoadCard', 'bm-queue-load-card');
+  // plugin.event.removeListener('QueueExit', 'bm-queue-exit');
+}
 
 declareIndexPlugin(onActivate, onDeactivate);
