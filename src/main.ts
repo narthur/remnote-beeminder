6010b81ec60a9ba849d50a863ab213caa59d01e3
Nathan Arthur <nathan@nathanarthur.com>
2023-04-11 09:17:57 -0400

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,53 @@
-const params = new URLSearchParams(window.location.search);
-const root = document.getElementById("root");
+import {
+  declareIndexPlugin,
+  ReactRNPlugin,
+  WidgetLocation,
+} from "@remnote/plugin-sdk";
 
-if (!root) throw new Error("No root element found");
+const params = new URLSearchParams(window.location.search);
+
+function initPlugin() {
+  // const { initReact } = window.__plugin;
+
+  // const root = document.getElementById("root");
+
+  // if (!root) throw new Error("No root element found");
+
+  // initReact(window.__plugin, root, window.location.origin);
+
+  async function onActivate(plugin: ReactRNPlugin) {
+    console.log("custom onActivate");
+
+    await plugin.settings.registerStringSetting({
+      id: "name",
+      title: "Name",
+      defaultValue: "Bobe",
+    });
+
+    await plugin.app.registerWidget(
+      "hello-world",
+      WidgetLocation.RightSidebar,
+      {
+        dimensions: {
+          width: 300,
+          height: "auto",
+        },
+      }
+    );
+  }
+
+  async function onDeactivate() {
+    console.log("custom onDeactivate");
+    // TODO
+  }
+
+  console.log("declaring index plugin");
+  declareIndexPlugin(onActivate, onDeactivate);
+}
 
 if (params.has("widgetName")) {
   const widgetName = params.get("widgetName");
-  import(`./widgets/${widgetName}.tsx`).then(() => {
-    const { initReact } = window.__plugin;
-    initReact(window.__plugin, root, window.location.origin);
-  });
+  import(`./widgets/${widgetName}.tsx`).then(initPlugin);
 } else {
   document.body.innerHTML =
     "No widget name provided. Please append ?widgetName=yourWidgetName to the URL.";
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name]-sandbox.js",
+      },
+    },
+  },
 });
