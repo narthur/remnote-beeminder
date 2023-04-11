4abff7eaff2c09f0f95bc53781b4b751fcab6c85
Nathan Arthur <nathan@nathanarthur.com>
2023-04-11 13:57:55 -0400

    post review count to beeminder

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,10 +1,4 @@
-import {
-  AppEventListerKey,
-  AppEvents,
-  declareIndexPlugin,
-  ReactRNPlugin,
-  WidgetLocation,
-} from '@remnote/plugin-sdk';
+import { AppEvents, declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
 import '../style.css';
 import '../App.css';
 import axios from 'axios';
@@ -27,80 +21,47 @@ async function onActivate(plugin: ReactRNPlugin) {
     defaultValue: '',
   });
 
-  const bmuser = await plugin.settings.getSetting('bmuser');
-  const bmtoken = await plugin.settings.getSetting('bmtoken');
-  const url = `https://www.beeminder.com/api/v1/users/${bmuser}/goals.json?auth_token=${bmtoken}`;
-  const { data } = await axios.get(url);
-  const slugs = data.map((g: Goal) => g.slug);
-
-  slugs.sort();
-
-  const options = slugs.map((s: string) => ({ key: s, value: s, label: s }));
-
-  await plugin.settings.registerDropdownSetting({
+  await plugin.settings.registerStringSetting({
     id: 'goal-reviews',
     title: 'Beeminder goal for review count',
-    options,
+    defaultValue: '',
   });
 
-  await plugin.settings.registerDropdownSetting({
+  await plugin.settings.registerStringSetting({
     id: 'goal-edits',
     title: 'Beeminder goal for edit count',
-    options,
+    defaultValue: '',
   });
-
-  // // A command that inserts text into the editor if focused.
-  // await plugin.app.registerCommand({
-  //   id: 'editor-command',
-  //   name: 'Editor Command',
-  //   action: async () => {
-  //     plugin.editor.insertPlainText('Hello World!');
-  //   },
-  // });
-
-  // // Show a toast notification to the user.
-  // await plugin.app.toast("I'm a toast!");
 
   // Register a sidebar widget.
-  await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
-    dimensions: { height: 'auto', width: '100%' },
-  });
+  // await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
+  //   dimensions: { height: 'auto', width: '100%' },
+  // });
 
   // Register event handlers
   plugin.event.addListener(AppEvents.QueueCompleteCard, undefined, async (e) => {
-    plugin.app.toast('Card completed!');
-    console.log('QueueCompleteCard', e);
-    console.log({
-      streak: '',
-      remaining: '',
+    const yyyymmdd = new Date().toISOString().split('T')[0].replace(/-/g, '');
+    const prev: number = (await plugin.storage.getSynced(yyyymmdd)) || 0;
+    const next = prev + 1;
+
+    await plugin.storage.setSynced(yyyymmdd, next);
+
+    const bmuser = await plugin.settings.getSetting('bmuser');
+    const bmtoken = await plugin.settings.getSetting('bmtoken');
+    const goalReviews = await plugin.settings.getSetting('goal-reviews');
+
+    if (!goalReviews || !bmuser || !bmtoken) return;
+
+    const url = `https://www.beeminder.com/api/v1/users/${bmuser}/goals/${goalReviews}/datapoints.json?auth_token=${bmtoken}`;
+
+    axios.post(url, {
+      daystamp: yyyymmdd,
+      value: next,
+      comment: 'via RemNote Beeminder plugin',
     });
   });
-
-  plugin.event.addListener(AppEvents.QueueEnter, undefined, async (e) => {
-    plugin.app.toast('Queue entered!');
-    console.log('QueueEnter', e);
-  });
-
-  plugin.event.addListener(AppEvents.QueueLoadCard, undefined, async (e) => {
-    plugin.app.toast('Card loaded!');
-    console.log('QueueLoadCard', e);
-  });
-
-  plugin.event.addListener(AppEvents.QueueExit, undefined, async (e) => {
-    plugin.app.toast('Queue exited!');
-    console.log('QueueExit', e);
-  });
-
-  // useAPIEventListener(AppEvents.QueueCompleteCard, 'bm_queue_complete_card', async (e) => {
-  //   console.log('QueueCompleteCard', e);
-  // });
 }
 
-async function onDeactivate(plugin: ReactRNPlugin) {
-  // plugin.event.removeListener('QueueCompleteCard', 'bm-queue-complete-card');
-  // plugin.event.removeListener('QueueEnter', 'bm-queue-enter');
-  // plugin.event.removeListener('QueueLoadCard', 'bm-queue-load-card');
-  // plugin.event.removeListener('QueueExit', 'bm-queue-exit');
-}
+async function onDeactivate(plugin: ReactRNPlugin) {}
 
 declareIndexPlugin(onActivate, onDeactivate);
